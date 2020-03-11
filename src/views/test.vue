5107e31b88bbd5fdf829b7ab4f0f5e78228d66e6
丁爽 <dingshuang@sunlands.com>
2020-03-11 19:52:03 +0800

    add test

--- src/views/test.vue
+++ src/views/test.vue
@@ -15,19 +15,25 @@
       </div>
       <div class="info-item">
           <label>openid</label>
-          <p>{{route}}</p>
+          <p></p>
       </div>
   </div>
 </template>
 <script>
+import axios from 'axios'
+const APPID = 'wx1ae5569fd1bd1e3e'
+const SECRET = '22437a698a6bac6837a866d723c9a261'
+
 export default {
     data () {
         return {
-            route: this.$route
         }
     },
     created () {
-        console.log('router', this.$route)
+        const code = this.$route.query.code
+        console.log('router', this.$route.query.code)
+        let url = `https://api.weixin.qq.com/sns/oauth2/access_token?appid=${APPID}&secret=${SECRET}&code=${code}&grant_type=authorization_code`
+        axios.get(url)
     }
 }
 </script>
